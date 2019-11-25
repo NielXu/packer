868a98a5079b356435cbf15d9252bce1a2b6e7c6
Daniel Xu <daniel.xu@fleetcomplete.com>
2019-11-25 16:51:27 -0500

    Added document for glue

--- builder/glue.js
+++ builder/glue.js
@@ -5,10 +5,21 @@
  */
 const fs = require('fs');
 
+/**
+ * Count the number of whitespaces before first character in string,
+ * return an integer.
+ * 
+ * @param {String} s String
+ */
 function countWhitespaceBefore(s) {
     return s.search(/\S|$/);
 }
 
+/**
+ * Return a string that fill with whitespaces with given length
+ * 
+ * @param {Number} count Integer
+ */
 function prependWhitespace(count) {
     let space = "";
     for(var i=0;i<count;i++) {
@@ -18,6 +29,22 @@ function prependWhitespace(count) {
 }
 
 module.exports = {
+    /**
+     * Find the glue notation inside the file and replace them
+     * by the given strings. The search variable should be an object
+     * with format `{tagName: replaceWith}`. As long as the tag is found,
+     * that line will be replaced with the given string. If the `replaceWith`
+     * is an array, every element of the array will be added in each line.
+     * If the notation is presented but it is not what we are searching for,
+     * that line will be removed.
+     * 
+     * The glue notation depends on lanaguage, usually it will be the comment
+     * character + !Glue, for example, in python it will be `#!Glue@someTagName`
+     * 
+     * @param {String} lang Language: python|py, javascript|js
+     * @param {String} file File location
+     * @param {Object} search What to search for and replace with
+     */
     glueReplace: function(lang, file, search) {
         const content = fs.readFileSync(file, 'utf-8').split("\n");
         let newContent = [];
